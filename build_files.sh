46e11c5fb3a3ff0b880ce3cca8c1e8914e8bc900
shzfahad <viperfahad@gmail.com>
2023-06-24 18:40:51 +0600

    Deploy Commit

---
+++ build_files.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Build static files (if applicable)
+python manage.py collectstatic --noinput
+
+# Run database migrations (if applicable)
+python manage.py migrate
+
+# Build any additional files or perform other build tasks here
+
+# Start the Django server
+python manage.py runserver 0.0.0.0:$PORT
