945e5306f4b449c7a112db3e3c717e203883926a
shzfahad <viperfahad@gmail.com>
2023-06-24 20:37:05 +0600

    SDA

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # Install dependencies
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 # Build static files (if applicable)
 python manage.py collectstatic --noinput
