c195f76d0c88facc28f9b31d1230007e9ae6a7fd
shzfahad <viperfahad@gmail.com>
2023-06-24 20:55:48 +0600

    asasasdasdwghsu

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env bash
+
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
 echo "BUILD FINISHED"
